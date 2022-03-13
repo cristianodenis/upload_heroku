18a93c5cc18c4b0795f7d11dc0ca1c7101496e1b
cristianodenis <cristianodenis62@gmail.com>
2022-03-13 17:38:49 -0300

    update

--- backend-com-mongodb/src/config/database.js
+++ backend-com-mongodb/src/config/database.js
@@ -3,5 +3,6 @@ const mongoose = require('mongoose');
 //const url = 'mongodb://localhost:27017/todo';
 const url = process.env.MONGO_URL;
 mongoose.connect(url,{useNewUrlParser: true});
+console.log("testando " + process.env.MONGO_URL);
 
 module.exports = mongoose; 
