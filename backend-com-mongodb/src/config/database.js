f12a8a35cdd07f64dd182e7a36f7d3f2b47365a0
cristianodenis <cristianodenis62@gmail.com>
2022-03-13 16:44:40 -0300

    update

--- backend-com-mongodb/src/config/database.js
+++ backend-com-mongodb/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //const url = 'mongodb://localhost:27017/todo';
-const url = 'https://deploy-backend-tarefas.herokuapp.com/';
+const url = 'mongodb+srv://deploy:uploaddeploy@cluster0.ofm2z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose.connect(url,{useNewUrlParser: true});
 
 module.exports = mongoose; 
